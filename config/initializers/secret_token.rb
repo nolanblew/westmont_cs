4fa5453a5258f8ef79da7adf44f0cf1627f46fd5
Nolan Blew <nolanblew@hotmail.com>
2012-03-29 13:05:38 -0700

    Added a home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WestmontCs::Application.config.secret_token = '3f4e72156ffa14f598d0d1d69ddf89b5f9b2b34564ed06fd9a006fc477ef6e4ddcad3945a1e2e35b5ca751eff2f70952fa2ff1ad0dbefc2ab97ba683935e0fdd'
